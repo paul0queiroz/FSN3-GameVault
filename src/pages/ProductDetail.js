48edecd36cdfd57089571937372ac74c474a8c5c
Thiago Barbosa <f.thiago.bq@gmail.com>
2025-07-12 18:35:45 -0300

    Implementação do controle de quantidade

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,31 +1,45 @@
 import './ProductDetail.css';
 
+import { useState } from 'react';
 import { useParams, Navigate, useNavigate } from 'react-router-dom';
 import products from '../data/products';
 
-import { Button, Container, Image } from 'react-bootstrap';
+import { ButtonGroup, Button, Container, Image } from 'react-bootstrap';
 
-function ProductDetail(){
+function ProductDetail() {
+    const [quantity, setQuantity] = useState(1);
     const navigate = useNavigate()
     const productId = useParams().id;
-    const product = getProductByID( productId );
-    
-    if(product === undefined)
-        return <Navigate to='/404'/>;
+    const product = getProductByID(productId);
 
-    return(
+    const incrementQuantity = () => setQuantity(quantity<99? quantity + 1 : quantity);
+    const decrementQuantity = () => setQuantity(quantity>1? quantity - 1 : quantity);
+
+    if (product === undefined)
+        return <Navigate to='/404' />;
+
+    return (
         <>
-            <Button onClick={() => navigate('/produto/' + (Number(productId)-1))}>Anterior</Button>
-            <Button onClick={() => navigate('/produto/' + (Number(productId)+1))}>Próximo</Button>
+            <Button onClick={() => navigate('/produto/' + (Number(productId) - 1))}>Anterior</Button>
+            <Button onClick={() => navigate('/produto/' + (Number(productId) + 1))}>Próximo</Button>
             <Container fluid id='product-detail' className="d-flex flex-column flex-md-row gap-1 gap-md-3 p-3">
-                <Image src={product.image} className='border border-dark' id="product-image" alt={product.name}/>
+                <Image src={product.image} className='border border-dark' id="product-image" alt={product.name} />
                 <Container fluid className='d-flex flex-column' id="product-side">
-                    <h1 className="display-5 fs-4">{product.name}</h1>
+                    <h1 className="display-5 fs-4" id="product-title">{product.name}</h1>
                     <Container id='product-description' className='m-0 p-0'>
                         {product.description}
                     </Container>
-                    <h3>R$ {product.price.replace(".", ",")}</h3>
-                    <Button>Adicionar ao carrinho</Button>
+                    <h3 className="product-price">R$ {product.price.replace(".", ",")}</h3>
+                    <Container flex className='d-flex flex-row justify-content-center gap-1'>
+                        <ButtonGroup aria-label="Quantity Control" id="control-quantity">
+                            <Button variant="light" onClick={decrementQuantity}>-</Button>
+                            <span>{String(quantity).padStart(2, '0')}</span>
+                            <Button variant="light" onClick={incrementQuantity}>+</Button>
+                        </ButtonGroup>
+
+                        <Button>Adicionar ao carrinho</Button>
+                    </Container>
+
                 </Container>
             </Container>
         </>
@@ -33,8 +47,8 @@ function ProductDetail(){
 }
 
 
-function getProductByID( id ){
-    const product = products.find( (product) => Number(product.id) === Number(id) );
+function getProductByID(id) {
+    const product = products.find((product) => Number(product.id) === Number(id));
     return product;
 }
 
