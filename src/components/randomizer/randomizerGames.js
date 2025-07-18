3fbe8e8e86db93738c6d1dfe82b6d78e81eefe4c
Thiago Barbosa <f.thiago.bq@gmail.com>
2025-07-18 08:03:29 -0300

    Limpando o repositório

--- src/components/randomizer/randomizerGames.js
+++
@@ -1,6 +0,0 @@
-import products from "../../data/products";
-
-export default function randomizerGames() {
-  const index = Math.floor(Math.random() * products.length);
-  return products[index];
-}
